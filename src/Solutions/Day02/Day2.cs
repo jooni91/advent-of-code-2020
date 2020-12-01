b211b7e44a3e32f085f13c470077a4a5c65c5bdd
Joonas Griesinger <griesinger.joonas@gmail.com>
2020-12-01 18:26:20 +0200

    Prepare for day 2.

---
+++ src/Solutions/Day02/Day2.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+using static AdventOfCode2020.Utilities.InputLoader;
+
+namespace AdventOfCode2020.Solutions.Day02
+{
+    public class Day2 : DayBase
+    {
+        protected override string Day => "02";
+
+        protected override string PartOne(string input)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override string PartTwo(string input)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
