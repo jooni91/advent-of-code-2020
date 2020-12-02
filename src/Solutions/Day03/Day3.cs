7caf355c4dc28c3f0f09eb456bdd1d5401cca516
Joonas Griesinger <griesinger.joonas@gmail.com>
2020-12-02 13:11:18 +0200

    Prepared for day 3.

---
+++ src/Solutions/Day03/Day3.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using static AdventOfCode2020.Utilities.InputLoader;
+
+namespace AdventOfCode2020.Solutions.Day03
+{
+    public class Day3 : DayBase
+    {
+        protected override string Day => "03";
+
+        protected override string PartOne(string input)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override string PartTwo(string input)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ tests/Solutions/Day03/Day3Tests.cs
@@ -0,0 +1,33 @@
+﻿using Xunit;
+
+namespace AdventOfCode2020.Tests.Solutions.Day03
+{
+    public class Day3Tests
+    {
+        //[Fact]
+        //public void PartOne_ShouldReturn_ExpectedValue()
+        //{
+        //    // Arrange
+        //    var daySolution = new AdventOfCode2020.Solutions.Day03.Day3();
+
+        //    // Act
+        //    var result = daySolution.GetResult(Part.One);
+
+        //    // Assert
+        //    Assert.Equal("548", result);
+        //}
+
+        //[Fact]
+        //public void PartTwo_ShouldReturn_ExpectedValue()
+        //{
+        //    // Arrange
+        //    var daySolution = new AdventOfCode2020.Solutions.Day03.Day3();
+
+        //    // Act
+        //    var result = daySolution.GetResult(Part.Two);
+
+        //    // Assert
+        //    Assert.Equal("502", result);
+        //}
+    }
+}
