77aba1daa12291d6fcdc479e1de55c3ef1b54e8f
Joonas Griesinger <griesinger.joonas@gmail.com>
2020-12-06 19:24:17 +0200

    Prepared for day 7.

---
+++ tests/Solutions/Day07/Day7Tests.cs
@@ -0,0 +1,33 @@
+﻿using Xunit;
+
+namespace AdventOfCode2020.Tests.Solutions.Day07
+{
+    public class Day7Tests
+    {
+        //[Fact]
+        //public void PartOne_ShouldReturn_ExpectedValue()
+        //{
+        //    // Arrange
+        //    var daySolution = new AdventOfCode2020.Solutions.Day07.Day7();
+
+        //    // Act
+        //    var result = daySolution.GetResult(Part.One);
+
+        //    // Assert
+        //    Assert.Equal("6590", result);
+        //}
+
+        //[Fact]
+        //public void PartTwo_ShouldReturn_ExpectedValue()
+        //{
+        //    // Arrange
+        //    var daySolution = new AdventOfCode2020.Solutions.Day07.Day7();
+
+        //    // Act
+        //    var result = daySolution.GetResult(Part.Two);
+
+        //    // Assert
+        //    Assert.Equal("3288", result);
+        //}
+    }
+}
