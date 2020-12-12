ee5cd946f35ed5138a2b4040ae682754a1168f34
Joonas Griesinger <griesinger.joonas@gmail.com>
2020-12-12 22:02:46 +0200

    Prepared for day 13.

---
+++ src/Solutions/Day13/Day13.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using static AdventOfCode2020.Utilities.InputLoader;
+
+namespace AdventOfCode2020.Solutions.Day13
+{
+    public class Day13 : DayBase
+    {
+        protected override string Day => "13";
+
+        protected override string PartOne(string input)
+        {
+            return base.PartOne(input);
+        }
+
+        protected override string PartTwo(string input)
+        {
+            return base.PartTwo(input);
+        }
+    }
+}
---
+++ tests/Solutions/Day13/Day13Tests.cs
@@ -0,0 +1,33 @@
+﻿using Xunit;
+
+namespace AdventOfCode2020.Tests.Solutions.Day13
+{
+    public class Day13Tests
+    {
+        //[Fact]
+        //public void PartOne_ShouldReturn_ExpectedValue()
+        //{
+        //    // Arrange
+        //    var daySolution = new AdventOfCode2020.Solutions.Day13.Day13();
+
+        //    // Act
+        //    var result = daySolution.GetResult(Part.One);
+
+        //    // Assert
+        //    Assert.Equal("2228", result);
+        //}
+
+        //[Fact]
+        //public void PartTwo_ShouldReturn_ExpectedValue()
+        //{
+        //    // Arrange
+        //    var daySolution = new AdventOfCode2020.Solutions.Day13.Day13();
+
+        //    // Act
+        //    var result = daySolution.GetResult(Part.Two);
+
+        //    // Assert
+        //    Assert.Equal("42908", result);
+        //}
+    }
+}
